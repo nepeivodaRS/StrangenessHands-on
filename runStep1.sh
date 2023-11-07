e8e6f393527bc09983fde9d7b88c6c14788dd790
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-11-07 18:15:29 +0100

    Initial commit

---
+++ runStep1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# log file where the terminal output will be saved
+STEP="step1"
+LOGFILE="log-${STEP}.txt"
+
+# directory of this script
+DIR_THIS="$(dirname "$(realpath "$0")")"
+
+OPTION="-b --configuration json:/$DIR_THIS/jsonConfig/$STEP.json"
+
+o2-analysis-timestamp ${OPTION} \
+| o2-analysis-event-selection ${OPTION} \
+| o2-analysis-bc-converter ${OPTION} \
+| o2-analysis-tracks-extra-converter  ${OPTION} \
+| o2-analysis-lf-lambdakzerobuilder ${OPTION} \
+| o2-analysistutorial-lf-strangeness-$STEP ${OPTION} \
+> "$LOGFILE" 2>&1
+
+# report status
+rc=$?
+if [ $rc -eq 0 ]; then
+  echo "No problems!"
+  mkdir -p "$DIR_THIS/results/$STEP"
+  mv "AnalysisResults.root" "$DIR_THIS/results/$STEP/AnalysisResults.root"
+else
+  echo "Error: Exit code $rc"
+  echo "Check the log file $LOGFILE"
+  exit $rc
+fi
