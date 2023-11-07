2d5775ad724e48182f55c8499c7237afeba82615
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-11-07 21:31:55 +0100

    rearrange

--- runStep0.sh
+++ runStep0.sh
@@ -6,7 +6,7 @@ LOGFILE="log-${STEP}.txt"
 # directory of this script
 DIR_THIS="$(dirname "$(realpath "$0")")"
 
-OPTION="-b --configuration json:/$DIR_THIS/jsonConfig/$STEP.json"
+OPTION="-b --configuration json:/$DIR_THIS/jsonConfigs/$STEP.json"
 
 o2-analysis-timestamp ${OPTION} \
 | o2-analysis-event-selection ${OPTION} \
@@ -23,7 +23,7 @@ if [ $rc -eq 0 ]; then
   echo "No problems!"
   mkdir -p "$DIR_THIS/results/$STEP"
   mv "AnalysisResults.root" "$DIR_THIS/results/$STEP/AnalysisResults.root"
-  mv "dpl-config.json" "$DIR_THIS/jsonConfig/$STEP.json"
+  mv "dpl-config.json" "$DIR_THIS/jsonConfigs/$STEP.json"
 else
   echo "Error: Exit code $rc"
   echo "Check the log file $LOGFILE"
